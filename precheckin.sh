f25d13621d1d3e1b9b88109b5359fd12f92181df
Niels Breet <niels.breet@jolla.com>
2021-01-29 14:39:37 +0200

    [aarch64] Fix aarch64 link. JB#51574

--- precheckin.sh
+++ precheckin.sh
@@ -2,5 +2,10 @@
 ARCHS="armv7hl aarch64 i486"
 
 for x in $ARCHS; do
-	sed "s/@ARCH@/$x/g" cross-as-template.spec > cross-$x-as.spec
+	if [ $x = "armv7hl" ]
+        then
+		sed "s/@ARCH@/$x/g;s/@ABI@/gnueabi/g" cross-as-template.spec > cross-$x-as.spec
+        else
+                sed "s/@ARCH@/$x/g;s/@ABI@/gnu/g" cross-as-template.spec > cross-$x-as.spec
+        fi
 done
